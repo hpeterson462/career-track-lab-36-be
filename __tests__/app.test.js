517e916b8b2e01812f92afc3dcdd0ff1ed3a98ef
Heather Peterson <hpeterson462@gmail.com>
2020-11-06 08:59:23 -0800

    find all recipes test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,7 @@ const Recipe = require('../lib/models/recipe');
 require('../data/data-helper');
 
 describe('recipe routes', () => {
-  it('creates a recipe VIA POST', () => {
+  it('creates a recipe via POST', () => {
     return request(app)
       .post('/api/v1/recipes')
       .send({
@@ -19,6 +19,28 @@ describe('recipe routes', () => {
           ingredients: ['2 Tbsp almond flour', '2 Tbsp protein powder', '2 Tbsp sugar', '1 Tbsp coconut oil', '1 Tbsp cocoa powder', '1 egg', '1/4 tsp baking soda'],
           directions: 'Mix dry ingredients. Add beaten egg and melted oil. Grease ramekin/mug and fill with an inch of empty space to rise. Microwave on high for 2 minutes.'
         });
+      });
+  });
+
+  it('finds all recipes via GET', async () => {
+    const recipes = await Promise.all([
+      Recipe.insert({
+        id: 1,
+        name: 'chocolate mug cake',
+        ingredients: ['2 Tbsp almond flour', '2 Tbsp protein powder', '2 Tbsp sugar', '1 Tbsp coconut oil', '1 Tbsp cocoa powder', '1 egg', '1/4 tsp baking soda'],
+        directions: 'Mix dry ingredients. Add beaten egg and melted oil. Grease ramekin/mug and fill with an inch of empty space to rise. Microwave on high for 2 minutes.'
+      }),
+      Recipe.insert({
+        name: 'tahini ginger mug cake',
+        ingredients: ['3 Tbsp almond flour', '2 Tbsp tahini butter', '2 Tbsp protein powder', '2 Tbsp sugar', '1 Tbsp coconut oil', '1 egg', '1/4 tsp ginger powder', '1/4 tsp baking soda'],
+        directions: 'Mix dry ingredients. Add beaten egg and melted oil. Grease ramekin/mug and fill with an inch of empty space to rise. Microwave on high for 2 minutes.'
+      })
+    ]);
+
+    return request(app)
+      .get('/api/v1/recipes')
+      .then(res => {
+        expect(res.body).toEqual(expect.arrayContaining(recipes));
       });
   });
 
