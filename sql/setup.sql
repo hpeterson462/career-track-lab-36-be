b356e8d6f90bd0ca43ba8e33cfac592c346f902c
Heather Peterson <hpeterson462@gmail.com>
2020-11-05 16:39:07 -0800

    model

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS recipes;
 
 CREATE TABLE recipes (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL,
   ingredients TEXT NOT NULL,
   directions TEXT NOT NULL
 );
