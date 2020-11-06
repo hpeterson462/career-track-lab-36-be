5be8f281c3953a7919601ed9b541ae527a00083a
Heather Peterson <hpeterson462@gmail.com>
2020-11-05 19:18:38 -0800

    testing progress

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,19 +1,26 @@
-const fs = require('fs');
-const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const Recipe = require('../lib/models/recipe');
+require('../data/data-helper');
 
 describe('recipe routes', () => {
-  beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
-  });
-
   it('creates a recipe VIA POST', () => {
     return request(app)
       .post('/api/v1/recipes')
       .send({
         name: 'chocolate mug cake',
-        ingredients: ''
+        ingredients: ['2 Tbsp almond flour', '2 Tbsp protein powder', '2 Tbsp sugar', '1 Tbsp coconut oil', '1 Tbsp cocoa powder', '1 egg', '1/4 tsp baking soda'],
+        directions: 'Mix dry ingredients. Add beaten egg and melted oil. Grease ramekin/mug and fill with an inch of empty space to rise. Microwave on high for 2 minutes.'
       })
-  })
+      .then(res => {
+        expect(res.body).toEqual({
+          id: expect.any(String),
+          name: 'chocolate mug cake',
+          ingredients: ['2 Tbsp almond flour', '2 Tbsp protein powder', '2 Tbsp sugar', '1 Tbsp coconut oil', '1 Tbsp cocoa powder', '1 egg', '1/4 tsp baking soda'],
+          directions: 'Mix dry ingredients. Add beaten egg and melted oil. Grease ramekin/mug and fill with an inch of empty space to rise. Microwave on high for 2 minutes.'
+        });
+      });
+  });
+
+
 });
---
+++ data/recipeData.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    name: 'chocolate mug cake',
+    ingredients: ['2 Tbsp almond flour', '2 Tbsp protein powder', '2 Tbsp sugar', '1 Tbsp coconut oil', '1 Tbsp cocoa powder', '1 egg', '1/4 tsp baking soda'],
+    directions: 'Mix dry ingredients. Add beaten egg and melted oil. Grease ramekin/mug and fill with an inch of empty space to rise. Microwave on high for 2 minutes.'
+  },
+  {
+    name: 'tahini ginger mug cake',
+    ingredients: ['3 Tbsp almond flour', '2 Tbsp tahini butter', '2 Tbsp protein powder', '2 Tbsp sugar', '1 Tbsp coconut oil', '1 egg', '1/4 tsp ginger powder', '1/4 tsp baking soda'],
+    directions: 'Mix dry ingredients. Add beaten egg and melted oil. Grease ramekin/mug and fill with an inch of empty space to rise. Microwave on high for 2 minutes.'
+  }
+];
